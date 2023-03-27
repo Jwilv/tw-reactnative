3c562acdb592df8d0670aa90c030e7be988a9f49
Matias Rolon <matiasrolon1301@gmail.com>
2023-03-27 14:01:21 -0300

    Creacion de los estilos de la publicacion

--- Navigation.js
+++ Navigation.js
@@ -1,7 +1,9 @@
-// import 'react-native-gesture-handler';
-// import React from "react";
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 
 // //screens
@@ -9,19 +11,45 @@ import HomeScreen from "./screens/HomeScreen";
 import SearchScreen from "./screens/SearchScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        tabBarActiveTintColor: 'blue',
+      }}>
+      <Tab.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({color, size})=>(
+            <MaterialCommunityIcons name="home" size={24} color={color} />
+          ), 
+        }}
+        />
+
+      <Tab.Screen 
+        name="Search" 
+        component={SearchScreen} 
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({color, size})=>(
+            <FontAwesome name="search" size={24} color={color} />
+          ), 
+        }}/>
+
+      <Tab.Screen 
+        name="Profile" 
+        component={ProfileScreen} 
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({color, size})=>(
+            <MaterialCommunityIcons name="account" size={24} color={color} />
+          ), 
+        }}/>
     </Tab.Navigator>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,} from 'react-native';
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+
+  },
+  contenedor:{
+    padding:10,
+    flexDirection: 'row',
+  },
+  linea:{
+    backgroundColor:'#B1B1B1',
+    width:'100%',
+    height: 1,
+  },
+  nombre:{
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  publicacion:{
+    marginLeft: 10,
+  }
+});
 
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <>
+    <View  style={styles.contenedor}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <View>
+        <Text style={styles.nombre}>Matias Rolon</Text>
+        <Text style={styles.publicacion}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </Text>
+      </View>
     </View>
+    <View style={styles.linea}></View>
+    </>
   );
 }
