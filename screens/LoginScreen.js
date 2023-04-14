9cc1d1dd82240f868bee2ff84550796f0894794f
Matias Rolón <matiasrolon1301@gmail.com>
2023-04-14 09:55:57 -0300

    Creacion del login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, Button} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import AppTextInput from "../components/AppTextInput";
+
 
 
 export default function LoginScreen({navigation}) {
   return (
-    <>
-        <Text>Login</Text>    
-        <Button
-          title='hola'
-          onPress={() => {navigation.navigate('Hoe')}}/>
-    </>
+    <SafeAreaView>
+      <View
+        style={{
+          padding: 20,
+        }}
+      >
+        <View
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 30,
+              color: '#1F41BB',
+              marginVertical: 30,
+            }}
+          >
+            Login here
+          </Text>
+          <Text
+            style={{              
+              fontSize: 20,
+              maxWidth: "60%",
+              textAlign: "center",
+            }}
+          >
+            Welcome back you've been missed!
+          </Text>
+        </View>
+        <View
+          style={{
+            marginVertical: 30,
+          }}
+        >
+          <AppTextInput placeholder="Email" />
+          <AppTextInput placeholder="Password" />
+        </View>
+
+        <View>
+          <Text
+            style={{
+              fontSize: 14,
+              color: '#1F41BB',
+              alignSelf: "flex-end",
+            }}
+          >
+            Forgot your password ?
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            padding: 20,
+            backgroundColor: '#1F41BB',
+            marginVertical: 30,
+            borderRadius: 10,
+            shadowColor: '#1F41BB',
+            shadowOffset: {
+              width: 0,
+              height: 10,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              textAlign: "center",
+              fontSize: 20,
+            }}
+          >
+            Sign in
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {navigation.navigate('Register')}}
+        >
+          <Text
+            style={{
+              color: '#000',
+              textAlign: "center",
+              fontSize: 14,
+            }}
+          >
+            Create new account
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+
+const styles = StyleSheet.create({});
