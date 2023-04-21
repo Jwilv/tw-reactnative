a406683005afd446a40fd296cd0d26adae1bffef
Matias Rolón <matiasrolon1301@gmail.com>
2023-04-21 10:28:05 -0300

    Creacion del fecht de helpers

---
+++ helpers/fecht.js
@@ -0,0 +1,50 @@
+
+// const baseUrl = import.meta.env.VITE_APP_URL_API
+const baseUrl = "http://localhost:8080"
+
+
+export const fetchWithoToken = async (endpoint, data, method = "GET") => {
+    const url = `${baseUrl}/${endpoint}`
+
+    if (method === "GET") {
+        const resp = await fetch(url)
+        return await resp.json()
+    } else {
+        const resp = await fetch(url, {
+            method: method,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        return await resp.json()
+    }
+
+}
+
+export const fetchToken = async (endpoint, data, method = "GET") => {
+    const url = `${baseUrl}/${endpoint}`
+    const token = localStorage.getItem('token')
+
+    if (method === "GET") {
+        const resp = await fetch(url, {
+            headers: {
+                "x-token": token
+            }
+        })
+        return await resp.json()
+    } else {
+        const resp = await fetch(url, {
+            method: method,
+            headers: {
+                "Content-type": "application/json",
+                "x-token": token
+            },
+            body: JSON.stringify(data)
+        })
+
+        return await resp.json()
+    }
+
+}
