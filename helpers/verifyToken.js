cb01381ba549aefc7bfd0a9225565ea94f375c27
Matias Rolón <matiasrolon1301@gmail.com>
2023-04-21 10:31:00 -0300

    Creacion del verifyToken de helpers

---
+++ helpers/verifyToken.js
@@ -0,0 +1,27 @@
+import jwtDecode from "jwt-decode"
+
+export const isExpired = (exp) => {
+    const expired = exp * 1000
+    const timeout = expired - Date.now()
+
+    if (timeout < 0) {
+        return true
+    }
+
+    return false
+}
+
+export const verifyToken = () => {
+
+    const token = localStorage.getItem("token")
+    try {
+        const { exp } = jwtDecode(token)
+        const status  = isExpired(exp)
+        console.log( "salio bien")
+        return !status
+    } catch (error) {
+        console.log("salio mal")
+        return false
+    }
+
+}
